a50692c7bd85c7420b71814c24c23805ac066e88
Gabriel <gmbarrera@gmail.com>
2015-08-26 19:51:59 -0300

    ups

--- src/WarriorExample1.java
+++ src/WarriorExample1.java
@@ -1,4 +1,42 @@
+import ia.battle.camp.FieldCell;
+import ia.battle.camp.Warrior;
+import ia.battle.camp.actions.Action;
+import ia.battle.camp.actions.Skip;
+import ia.exceptions.RuleException;
 
-public class WarriorExample1 {
+
+public class WarriorExample1 extends Warrior {
+
+	public WarriorExample1(String name, int health, int defense, int strength,
+			int speed, int range) throws RuleException {
+		super(name, health, defense, strength, speed, range);
+	}
+
+	@Override
+	public Action playTurn(long tick, int actionNumber) {
+		// TODO Auto-generated method stub
+		
+		return new Attack(celdaAAtacar);
+		
+		return new Skip();
+	}
+
+	@Override
+	public void wasAttacked(int damage, FieldCell source) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enemyKilled() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void worldChanged(FieldCell oldCell, FieldCell newCell) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
